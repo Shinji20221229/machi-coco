49d23483de422de30850fbca2d82fb7f4d989719
EC2 Default User <121584968+Shinji20221229@users.noreply.github.com>
2023-03-12 07:42:23 +0000

    新規登録・ログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: {
+  devise_for :users,skip: [:passwords], controllers: {
     sessions: 'users/sessions',
     passwords: 'users/passwords',
     registrations: 'users/registrations'
   }
 
-  devise_for :admins, controllers: {
+  devise_for :admins, skip: [:registrations, :passwords], controllers: {
     sessions: 'admins/sessions',
     passwords: 'admins/passwords',
     registrations: 'admins/registrations'
@@ -15,6 +15,21 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "users/homes#top"
+  namespace :admin do
+    root to: 'homes#top'
+    resources :users, only: [:index, :show, :edit, :update]
+
+  end
+  post 'users' => 'users#create'
+  get 'users/new'
+  get 'users/create'
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/destroy'
+
+
 
 
 end
