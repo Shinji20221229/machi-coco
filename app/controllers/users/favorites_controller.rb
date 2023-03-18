99265dbdc7f9eaf71744825bc00241e701e441dc
EC2 Default User <121584968+Shinji20221229@users.noreply.github.com>
2023-03-18 07:18:50 +0000

    commit

--- app/controllers/users/favorites_controller.rb
+++ app/controllers/users/favorites_controller.rb
@@ -1,11 +1,11 @@
-class FavoritesController < ApplicationController
+class Users::FavoritesController < ApplicationController
 
   def create
     tweet =Tweet.find(params[:tweet_id])
-    favorite = current_user.favorites.new(tweet_id: tweet.id)
+    favorites = current_user.favorites.new(tweet_id: tweet.id)
     favorites.save
     @tweet = Tweet.find(params[:tweet_id])
-    render "create"
+    redirect_to tweet_path(params[:tweet_id])
   end
 
   def destroy
@@ -13,7 +13,7 @@ class FavoritesController < ApplicationController
     favorite = current_user.favorites.find_by(tweet_id: tweet.id)
     favorite.destroy
     @tweet = Tweet.find(params[:tweet_id])
-    render "create"
+    redirect_to tweet_path(params[:tweet_id])
   end
 
 end
