1dfdeb0433a8b7e971d275c6fa65a354159925e7
EC2 Default User <121584968+Shinji20221229@users.noreply.github.com>
2023-04-02 11:54:11 +0000

    topページ編集完了

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   }
 
   devise_scope :user do
-    post '/homes/guest_sign_in', to: 'users/homes#guest_sign_in'
+    post '/guest_sign_in', to: 'users/homes#guest_sign_in'
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
